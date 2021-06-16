80683025e2478676189af1b9deba0278496d506e
Sarah <sarahmenezesbraga@hotmail.com>
2021-06-16 14:39:14 -0300

    Usando o ViaCEP e tratando os erros de retorno no json

---
+++ app/Services/ViaCEP.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Services;
+use Illuminate\Support\Facades\Http;
+
+class ViaCEP{
+    public function buscar(string $cep){
+        $url = sprintf('https://viacep.com.br/ws/%s/json/', $cep);
+
+        $resposta = Http::get($url);
+
+        if($resposta->failed()){
+            return false;
+        }
+
+       $dados = $resposta->json();
+
+       if(isset($dados['erro'])&& $dados['erro'] === true){ //tratando cep 99999 erro = true
+           return false;
+       }
+
+       return $dados;
+
+    }
+}
